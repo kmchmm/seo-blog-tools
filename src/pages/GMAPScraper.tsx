1d004ea92a4f03054efa248c594315d2cf1c4a6f
Melvin Soriano <melvin.akph@gmail.com>
2025-04-11 04:27:20 +0800

    feat: working changes to gmaps

--- src/pages/GMAPScraper.tsx
+++ src/pages/GMAPScraper.tsx
@@ -1,35 +1,81 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../assets/css/GMAPScraper.css'; // For styling the sidebar
+import { Button } from '../components/Button';
+import { Loading } from '../components/Loading';
+
+const COUNTIES = [
+  'alameda',
+  'amador',
+  'butte',
+  'calaveras',
+  'contra costa',
+  'el dorado',
+  'fresno',
+  'humboldt',
+  'imperial',
+  'kern',
+  'kings',
+  'los angeles',
+  'madera',
+  'marin',
+  'mendocino',
+  'merced',
+  'monterey',
+  'napa',
+  'nevada',
+  'orange',
+  'placer',
+  'riverside',
+  'sacramento',
+  'san bernardino',
+  'san diego',
+  'san francisco',
+  'san joaquin',
+  'san luis obispo',
+  'san mateo',
+  'santa barbara',
+  'santa clara',
+  'santa cruz',
+  'shasta',
+  'solano',
+  'sonoma',
+  'stanislaus',
+  'sutter',
+  'tehama',
+  'tulare',
+  'tuolumne',
+  'ventura',
+  'yolo'];
 
 const GMAPScraper: React.FC = () => {
-  const [newsArticles, setNewsArticles] = useState<any[]>([]);
+  const [mapResults, setMapResults] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
   const [keywords, setKeywords] = useState<string>('');
-  const [timeFilter, setTimeFilter] = useState<string>('a');
+  const [location, setLocation] = useState<string>('a');
 
-  const fetchNews = async (query: string, timeFilter: string) => {
+  const fetchMaps = async (keywords: string) => {
     setLoading(true);
     setError('');
     try {
-      const response = await axios.get(`http://localhost:3000/news/${query}`, {
+      const response = await axios.get(`http://localhost:8003`, {
         headers: {
-          'X-Time': timeFilter,
+          'X-Keywords': keywords,
         },
       });
 
       console.log('API Response:', response.data);
 
       const data = response.data;
-      if (data.deta && data.deta.length > 0) {
-        setNewsArticles(data.deta);
+      if (data.results && data.results.length > 0) {
+        setMapResults(data.results);
       } else {
         setError('No articles found for your search.');
       }
     } catch (err) {
       console.error('Error during API call:', err);
-      setError('An error occurred while fetching news.');
+      setError('An error occurred while fetching results.');
     } finally {
       setLoading(false);
     }
@@ -38,10 +84,10 @@ const GMAPScraper: React.FC = () => {
   const handleSearch = () => {
     if (keywords.trim() === '') {
       setError('Please enter a search keyword.');
-      setNewsArticles([]); // Clear any previous articles
+      setMapResults([]); // Clear any previous articles
     } else {
       setError(''); // Clear previous error if any
-      fetchNews(keywords, timeFilter);
+      fetchMaps(keywords);
     }
   };
 
@@ -53,27 +99,27 @@ const GMAPScraper: React.FC = () => {
           type="text"
           value={keywords}
           onChange={(e) => setKeywords(e.target.value)}
-          placeholder="Search for news"
+          placeholder="Keywords, Phrases, Sentences"
         />
         <select
-          value={timeFilter}
-          onChange={(e) => setTimeFilter(e.target.value)}
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
         >
-          <option value="a">Any</option>
-          <option value="h">Past Hour</option>
-          <option value="d">Past 24 Hours</option>
-          <option value="w">Past Week</option>
-          <option value="m">Past Month</option>
-          <option value="y">Past Year</option>
+          <option value='any' key='any'>Any</option>
+          {COUNTIES.map(county => 
+            <option value={county} key={county}>
+              {county.toUpperCase()}
+            </option>
+          )}
         </select>
-        <button onClick={handleSearch} disabled={loading} className="news-search-button">
+        <Button onClick={handleSearch} disabled={loading}>
           {loading ? 'Searching...' : 'Request'}
-        </button>
+        </Button>
       </div>
 
       {loading && (
         <div className="loading">
-          <div className="loading-spinner"></div>
+          <Loading />
         </div>
       )}
       
@@ -83,34 +129,38 @@ const GMAPScraper: React.FC = () => {
         <table>
           <thead>
             <tr>
-              <th>FEATURED</th>
               <th>TITLE</th>
-              <th>URL</th>
-              <th>DATE PUBLISHED</th>
+              <th>TYPE</th>
+              <th>ADDRESS</th>
+              <th>COUNTY</th>
+              <th>PHONE NUMBER</th>
+              <th>RATING</th>
+              <th>WEBSITE</th>
             </tr>
           </thead>
           <tbody>
-            {keywords.trim() !== '' && newsArticles.length === 0 && (
+            {keywords.trim() !== '' && mapResults.length === 0 && (
               <tr>
-                <td colSpan={4}>No articles found.</td>
+                <td colSpan={4}>No results found.</td>
               </tr>
             )}
-            {newsArticles.length > 0 &&
-              newsArticles.map((article, index) => {
+            {mapResults.length > 0 &&
+              mapResults.map((result, index) => {
                 return (
                   <tr key={index}>
-                    <td></td>
-                    <td>{article.website}</td>
+                    <td>{result.title}</td>
+                    <td>{result.type}</td>
+                    <td>{result.address}</td>
+                    <td>{result.county}</td>
+                    <td>{result.phone_number}</td>
+                    <td>{result.rating} {result.rating_count}</td>
                     <td className="news-url-holder">
                       <a
-                        href={article.url}
+                        href={result.website}
                         target="_blank"
                         rel="noopener noreferrer"
-                      >
-                        READ MORE
-                      </a>
+                      >{result.website}</a>
                     </td>
-                    <td>{article.date}</td>
                   </tr>
                 );
               })}
