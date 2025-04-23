e484bc902b0919433359b79851c19827f41fb452
Melvin Soriano <melvin.akph@gmail.com>
2025-04-24 06:58:12 +0800

    feat: separate mapresults and gmap scraping orders into their own component

---
+++ src/components/MapRecord.tsx
@@ -0,0 +1,68 @@
+const isDev = import.meta.env.MODE === 'development';
+
+export interface scrapeData {
+  address: string;
+  county: string;
+  details: string;
+  phone_number: string;
+  rating: string;
+  rating_count: string;
+  title: string;
+  type: string;
+  website: string;
+}
+
+export interface modifiedScrapeData extends scrapeData {
+  id: string;
+}
+
+export interface MapRecordProps extends modifiedScrapeData {
+  loading: boolean;
+  deleteGMapRequest: Function;
+}
+
+export const MapRecord = (mapProps: MapRecordProps) => {
+  return (
+    <tr key={`${mapProps.title}_${mapProps.address}`}>
+      <td>{mapProps.title}</td>
+      <td>{mapProps.type}</td>
+      <td className="capitalize">{mapProps.county}</td>
+      <td>{mapProps.address}</td>
+
+      <td>{(mapProps.phone_number.trim().toLowerCase() !== 'no contact number') ?
+        <a
+          href={`tel:${mapProps.phone_number}`}
+          target="_blank"
+          rel="noopener noreferrer">
+          {mapProps.phone_number}
+        </a> : '-----'          
+        }
+      </td>
+      <td>{mapProps.details}</td>
+      <td>{(mapProps.website.trim().toLowerCase() !== 'no links') ?
+        <a
+          className="line-clamp-3"
+          href={mapProps.website}
+          target="_blank"
+          rel="noopener noreferrer">
+          {mapProps.website}
+        </a> : '-----'          
+        }
+      </td>
+      {isDev && (
+        <td>
+          <button
+            disabled={mapProps.loading}
+            className={
+              mapProps.loading ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+            }
+            onClick={async () => {
+              mapProps.deleteGMapRequest(mapProps.id);
+            }}>
+            DELETE {mapProps.id}
+          </button>
+        </td>
+      )}
+    </tr>
+  );
+};
---
+++ src/components/OctoBitRecord.tsx
@@ -0,0 +1,77 @@
+import {
+    HTMLAttributes,
+  } from 'react';
+import clsx from 'clsx';
+
+const isDev = import.meta.env.MODE === 'development';
+
+interface OctoBitsPanelProps extends HTMLAttributes<HTMLDivElement> {
+  recordId: string;
+  desc: string;
+  requester: string;
+  status: string;
+  date: string;
+  workerId: string;
+  showMapResults: Function;
+  deleteAkOctobit: Function;
+  loading: boolean;
+}
+
+export const OctoBitRecord = (octoBitsProps: OctoBitsPanelProps) => {
+  return (
+    <div
+      onClick={() => {
+        octoBitsProps.showMapResults(
+          octoBitsProps.workerId,
+          octoBitsProps.status.trim().toUpperCase() === 'DONE'
+        );
+      }}
+      className={clsx(
+        'p-2 w-full cursor-pointer hover:scale-115',
+        'ease-in transition-transform duration-200'
+      )}>
+      <div
+        className={clsx(
+          'grid text-center p-4 rounded-md border-yellow-100 border',
+          isDev
+            ? 'grid-cols-[2fr_3fr_3fr_2fr_2fr_1fr]'
+            : 'grid-cols-[2fr_3fr_3fr_2fr_2fr]'
+        )}>
+        <div className="border-r">
+          <div>ID</div>
+          <div className="font-bold">{octoBitsProps.recordId}</div>
+        </div>
+
+        <div className="border-r">
+          <div>Description</div>
+          <div>{octoBitsProps.desc}</div>
+        </div>
+
+        <div className="border-r">
+          <div>Order by</div>
+          <div className="font-bold">{octoBitsProps.requester}</div>
+        </div>
+
+        <div className="font-bold flex items-center justify-center">
+          {octoBitsProps.status}
+        </div>
+
+        <div className="font-bold flex items-center justify-center">
+          {octoBitsProps.date.split(' ')[0]}
+        </div>
+        {isDev && (
+          <button
+            disabled={octoBitsProps.loading}
+            className={
+              octoBitsProps.loading ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+            }
+            onClick={async () => {
+              octoBitsProps.deleteAkOctobit(octoBitsProps.recordId);
+            }}>
+            DELETE
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
