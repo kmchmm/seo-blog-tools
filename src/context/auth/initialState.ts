cfd26e0e23b1724d1c5c8c1b6e38db7414626027
gabriel gonzales <gabrielgonzales.akph@gmail.com>
2025-06-13 02:49:58 +0800

    add types and initialState files

---
+++ src/context/auth/initialState.ts
@@ -0,0 +1,33 @@
+import { State } from './types';
+
+export const initUserData = {
+  id: 0,
+  profilegrid_id: 0,
+  username: '',
+  email: '',
+  birthdate: '',
+  full_name: '',
+  nickname: '',
+  cover_photo: '',
+  profile_picture: '',
+  gender: '',
+  department: '',
+  country: '',
+  position: '',
+  city: '',
+  about_me: '',
+  employment: '',
+  date_hired: '',
+  regularization_date: '',
+  date_created: '',
+  date_deleted: '',
+  active: false,
+};
+
+export const initialState: State = {
+  userData: initUserData,
+  setUserData: () => {},
+  logout: () => {},
+  toolsAccess: [],
+  setToolsAccess: () => {},
+};
---
+++ src/context/auth/types.ts
@@ -0,0 +1,40 @@
+export interface UserData {
+  id: number;
+  profilegrid_id: number;
+  username: string;
+  email: string;
+  birthdate: string;
+  full_name: string;
+  nickname: string;
+  cover_photo: string;
+  profile_picture: string;
+  gender: string;
+  department: string;
+  country: string;
+  position: string;
+  city: string;
+  about_me: string;
+  employment: string;
+  date_hired: string;
+  regularization_date: string;
+  date_created: string;
+  date_deleted: string;
+  active: boolean;
+}
+
+export type State = {
+  userData: UserData;
+  setUserData: (userData: UserData) => void;
+  logout: () => void;
+  toolsAccess: string[];
+  setToolsAccess: (tools: string[]) => void;
+};
+
+export interface ToolsResults {
+  id: number;
+  created_at: string;
+  department: string;
+  tool_access: {
+    access: string[];
+  };
+}
